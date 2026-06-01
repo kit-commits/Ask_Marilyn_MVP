3edceba16d442f7eb307c1859e00e14dd7cf6244
kit-commits <120128142+kit-commits@users.noreply.github.com>
2026-06-01 12:02:13 -0700

    Add Explanation Screen

---
+++ Explanation.swift
@@ -0,0 +1,55 @@
+//
+//  Explanation.swift
+//  
+//
+//  Created by Kaitlin Taylor on 6/1/26.
+//
+
+import SwiftUI
+
+struct Explanation: View {
+    @State private var showIllustration = false
+    
+    var body: some View {
+        
+        ZStack{
+            Color(red: 0.65, green: 0.65, blue: 0.877)
+                .ignoresSafeArea()
+        }
+        
+        VStack(spacing: 34){
+            Text(""Explanation?"")
+                .font(.custom("Playfair Display-Bold", size: 42))
+                .foregroundColor(Color(red: 0.15, green: 0.14, blue: 0.18))
+            
+                .multilineTextAlignment(.leading)
+            
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 60)
+               
+            Image(actor systemName: "IMG_8447.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 260, height: 420)
+                .opacity(showIllustration ? 1: 0)
+                .animation(.easeIn(duration: 1.5), value: showIllustration)
+                .padding()
+                .background(Color.A7A7DE)
+        }
+        Text("The goat has a 2/3 \n chance of winning.")
+        
+    }
+    
+    Button("Why"){
+        print(Proceed Forward)
+    }
+    .font("Playfair Display")
+    .fontWeight(.bold)
+    .fontSize(32)
+    .cornerRadius(24)
+    .fontcolor(5F5FA0)
+}
+
+#Preview {
+    ContentView ()
+}
